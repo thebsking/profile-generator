84f0503db8507e254f83e06d2bb5de4a528439b6
Brandon King <brandonscottking@gmail.com>
2021-04-03 19:54:32 -0400

    inquirer questions for other employee types

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 
 //inquirer questions
-const questionsStart =[
+const questionsMgr =[
     {
         type: 'input',
         message: `Please enter the Manager's name`,
@@ -28,15 +28,68 @@ const questionsStart =[
         type: 'input',
         message: `Please enter the Manager's office number`,
         name: 'mgrOffice'
+    },    
+]
+const questionsAll = [
+    {
+        type: 'input',
+        message: `Please enter the Employee's name`,
+        name: 'empName'
     },
     {
-        type: 'confirm',
-        message: 'would you like to enter another team member?',
-        name: 'playAgain'
+        type: 'input',
+        message: `Please enter the Employee's ID`,
+        name: 'empId'
     },
-    
+    {
+        type: 'input',
+        message: `Please enter the Employee's email address`,
+        name: 'empEmail'
+    },
+]
+const questionsEng = [
+    ...[questionsAll],
+    {
+        type: 'input',
+        name: 'github',
+        message: `Please enter the Employee's Github username`
+    }
+]
+const questionsIntern = [
+    ...[questionsAll],
+    {
+        name: 'school',
+        type: 'input',
+        message: `Please enter the employee's school`
+    }
 ]
 
+function newEmployee() {
+    inquirer
+        .prompt({name:'employeeType', type:'list', message:'Would you like to add another employee?',choices:['Engineer', 'Intern','No']})
+        .then((data)=>{
+            switch (data){
+                case 'Engineer':
+                    newEngineer();
+                    break;
+                case 'Intern':
+                    newIntern();
+                    break;
+                case 'No':
+                    return;
+
+            }
+        })
+};
+
 function init() {
-    
+    inquirer
+        .prompt(questionsMgr)
+        .then((data)=> {
+            const manager = new Manager(data.mgrName, data.mgrId, data.mgrEmail, data.mgrOffice);
+            console.log(manager)
+            newEmployee(); 
+        })
 }
+
+init();
