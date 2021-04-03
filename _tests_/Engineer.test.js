5c2f30783ff69353feac1f12804ee9f2afdfbe4d
Brandon King <brandonscottking@gmail.com>
2021-04-03 17:47:35 -0400

    completed engineer.test;

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,26 +1,31 @@
-const Employee = require('../lib/employee');
+const Engineer = require('../lib/engineer');
 
 describe('Employee class', () => {
     describe('getName', () => {
         it('getName returns employee name', () => {
-            expect(new Employee('David', '002', 'dave@email.com').getName()).toBe('David')
+            expect(new Engineer('David', '002', 'dave@email.com').getName()).toBe('David')
         })
     })
     
     describe('getId', () => {
         it('getId returns employee id', () => {
-            expect(new Employee('David', '002', 'dave@email.com').getId()).toBe('002')
+            expect(new Engineer('David', '002', 'dave@email.com','davedevs').getId()).toBe('002')
         })    
     })
 
     describe('getEmail', () => {
         it('getEmail returns employee email', () => {
-            expect(new Employee('David', '002', 'dave@email.com').getEmail()).toBe('dave@email.com')
+            expect(new Engineer('David', '002', 'dave@email.com','davedevs').getEmail()).toBe('dave@email.com')
         })    
     })
     describe('getRole', ()=> {
         it('getRole returns employee', () => {
-            expect(new Employee('David', '002', 'dave@email.com').getRole()).toBe('Employee')
+            expect(new Engineer('David', '002', 'dave@email.com','davedevs').getRole()).toBe('Engineer')
+        })
+    })
+    describe('getGithub', ()=> {
+        it('getGithub returns github username', () => {
+            expect(new Engineer('David', '002', 'dave@email.com','davedevs').getGithub()).toBe('davedevs')
         })
     })
 
